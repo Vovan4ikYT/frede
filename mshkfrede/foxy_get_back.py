8bb7cd6cbe3a8766f62b2906b49e01ce2fee8475
Vladimir Kuznetsow <116459828+Vovan4ikYT@users.noreply.github.com>
2023-05-07 12:14:26 +0300

    Add files via upload

---
+++ mshkfrede/foxy_get_back.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+class FoxyBack(Label):
+    def load(self, im):
+        im = Image.open(im)
+        self.cadr = 0
+        self.frames = []
+
+        for i in range(4):
+            self.frames.append(ImageTk.PhotoImage(im.copy()))
+            im.seek(i)
+
+        self.delay = 100
+
+        self.next_frame()
+
+    def unload(self):
+        self.config(image='')
+        self.frames = []
+
+    def next_frame(self):
+        if self.frames:
+            self.cadr += 1
+            self.cadr %= len(self.frames)
+            self.config(image=self.frames[self.cadr])
+            self.after(self.delay, self.next_frame)
